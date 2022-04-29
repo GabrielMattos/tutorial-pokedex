d3150ca950345a26c37a8d2eb1876c979d1bb644
Recriando Games <gabriel.mattos17@hotmail.com>
2022-04-29 18:34:57 -0300

    Pokemon data

--- src/app/components/pokemon-card/pokemon-card.component.ts
+++ src/app/components/pokemon-card/pokemon-card.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Pokemon } from 'src/model/Pokemon';
 
 @Component({
   selector: 'app-pokemon-card',
   templateUrl: './pokemon-card.component.html',
   styleUrls: ['./pokemon-card.component.sass']
 })
-export class PokemonCardComponent implements OnInit {
+export class PokemonCardComponent {
 
-  constructor() { }
+  @Input('pokemon')
+  public pokemon: Pokemon;
 
-  ngOnInit(): void {
+  public leadingZero(str: string | Number, size = 3): string {
+    let s = String(str);
+
+    while (s.length < (size || 2))
+    {
+      s = '0' + s;
+    }
+
+    return s;
   }
 
 }
--- src/app/components/pokemon-list/pokemon-list.component.ts
+++ src/app/components/pokemon-list/pokemon-list.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Type } from "src/model/Type";
+import { Pokemon } from "src/model/Pokemon";
+
+
 
 @Component({
   selector: 'app-pokemon-list',
   templateUrl: './pokemon-list.component.html',
   styleUrls: ['./pokemon-list.component.sass']
 })
-export class PokemonListComponent implements OnInit {
 
-  constructor() { }
+export class PokemonListComponent {
 
-  ngOnInit(): void {
-  }
-
+  public pokemons: Pokemon[] = [
+    {
+      image: 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/001.png',
+      number: 1,
+      name: 'Bulbasaur',
+      types: [
+        Type.Grass,
+        Type.Poison,
+      ],
+    },
+  ];
 }
+
